6040dffc0b5a3c370c6453ab788bcce98279bd18
Joshua Huelsman <joshuahuelsman@gmail.com>
2013-05-13 02:05:49 -0400

    Added one time warning about PocketTool's issues on Jellybean.

--- src/com/joshuahuelsman/pockettool/MainScreen.java
+++ src/com/joshuahuelsman/pockettool/MainScreen.java
@@ -10,8 +10,13 @@
  *******************************************************************************/
 package com.joshuahuelsman.pockettool;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -22,40 +27,75 @@ public class MainScreen extends Activity implements OnClickListener {
 	public ProgressDialog dialog;
 	boolean root = false;
 	private Button toolkit;
+
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		Button installdownload = (Button)findViewById(R.id.installdownloadbutton);
-		Button links = (Button)findViewById(R.id.linksbutton);
-		toolkit = (Button)findViewById(R.id.toolkitbutton);
-		Button leveleditor = (Button)findViewById(R.id.leveleditorbutton);
+
+		Button installdownload = (Button) findViewById(R.id.installdownloadbutton);
+		Button links = (Button) findViewById(R.id.linksbutton);
+		toolkit = (Button) findViewById(R.id.toolkitbutton);
+		Button leveleditor = (Button) findViewById(R.id.leveleditorbutton);
 		installdownload.setOnClickListener(this);
 		links.setOnClickListener(this);
 		toolkit.setOnClickListener(this);
 		leveleditor.setOnClickListener(this);
-		
+
+		if (android.os.Build.VERSION.SDK_INT >= 16) {
+			File jelly = new File(APKManipulation.ptdir, "jellybean");
+			if (!jelly.exists()) {
+				try {
+					File parent = jelly.getParentFile();
+					if (!parent.exists()) {
+						parent.mkdirs();
+					}
+					jelly.createNewFile();
+					AlertDialog.Builder builder = new AlertDialog.Builder(this);
+					builder.setPositiveButton(R.string.goto_settings,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									Intent intent = new Intent(MainScreen.this,
+											Settings.class);
+									startActivity(intent);
+								}
+							});
+					builder.setNegativeButton(R.string.ok,
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									dialog.dismiss();
+								}
+							});
+					builder.setMessage(R.string.jellybean_warning);
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
 
 	}
 
-	
-
 	public void onClick(View v) {
-		if(v.getId() == R.id.installdownloadbutton){
+		if (v.getId() == R.id.installdownloadbutton) {
 			TexturesSkinsActivity.MODE_DOWNLOAD = 1;
 			Intent i = new Intent(this, TexturesSkinsActivity.class);
 			startActivity(i);
-		}else if(v.getId() == R.id.linksbutton){
+		} else if (v.getId() == R.id.linksbutton) {
 			Intent i = new Intent(this, Links.class);
 			startActivity(i);
-		}else if(v.getId() == R.id.toolkitbutton){
+		} else if (v.getId() == R.id.toolkitbutton) {
 			Intent i = new Intent(this, ToolKit.class);
 			startActivity(i);
-		}else if(v.getId() == R.id.leveleditorbutton){
+		} else if (v.getId() == R.id.leveleditorbutton) {
 			Intent i = new Intent(this, LevelSelector.class);
 			startActivity(i);
 		}
-		
+
 	}
 }
